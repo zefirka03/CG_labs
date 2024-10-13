fde46bb8c0906b7c3dd0b63e60c4b40077f65e56
ka Kotey <newpontos@gmail.com>
2024-10-13 21:35:58 +0300

    lab 1

--- src/lab1.cpp
+++ src/lab1.cpp
@@ -1,19 +1,105 @@
 #include <iostream>
 #include "png_files.h"
 
-int main(){
-    auto point = read_png_file("file.png");
+#define ERROR 0
+#define OK 1
 
-    for(int y = 0; y < height; y++) {
-        png_bytep row = point[y];
-        for(int x = 0; x < width; x++) {
-            png_bytep px = &(row[x * 4]);
-            // Do something awesome for each pixel here...
-            //printf("%4d, %4d = RGBA(%3d, %3d, %3d, %3d)\n", x, y, px[0], px[1], px[2], px[3]);
-            px[0] = 0;
+using blend_func_t = png_byte(*)(png_byte, png_byte, png_byte);
+    
+png_byte default_blend_func(png_byte a, png_byte b, png_byte alpha) {
+    return a * (alpha / 255.f) + a * (1.f - alpha / 255.f);
+}
+
+int blend(
+    Image const& a,
+    Image const& b,
+    Image const& mask,
+    Image& out,
+    blend_func_t blend_func = default_blend_func
+){
+    if(
+        a.height != b.height ||
+        a.width != b.width ||
+        a.height != mask.width ||
+        a.width != mask.width
+    ) return ERROR;
+
+    if (!out.pixels) {
+        out.pixels = (png_bytep*)malloc(sizeof(png_bytep) * out.height);
+        for (int y = 0; y < out.height; y++)
+            out.pixels[y] = (png_byte*)malloc(png_get_rowbytes(a.png, a.info));
+    }
+
+    for(int y = 0; y < a.height; y++) {
+        png_bytep row_a = a.pixels[y];
+        png_bytep row_b = b.pixels[y];
+        png_bytep row_mask = mask.pixels[y];
+        png_bytep row_out = out.pixels[y];
+
+        for(int x = 0; x < a.width; x++) {
+            png_bytep px_a = &(row_a[x * 4]);
+            png_bytep px_b = &(row_b[x * 4]);
+            png_bytep px_mask = &(row_mask[x * 4]);
+            png_bytep px_out = &(row_out[x * 4]);
+
+            for(int i = 0; i < 4; ++i)
+                px_out[i] = blend_func(px_a[i], px_b[i], px_mask[3]);
         }
     }
-    write_png_file(point, "file2.png");
+
+    return OK;
+}
+
+int circle_image(
+    Image const& a,
+    Image& out
+) {
+    int radius = std::min(a.width, a.height) / 2;
+
+    if (!out.pixels) {
+        out.pixels = (png_bytep*)malloc(sizeof(png_bytep) * out.height);
+        for (int y = 0; y < out.height; y++)
+            out.pixels[y] = (png_byte*)malloc(png_get_rowbytes(a.png, a.info));
+    }
+
+    int center_w = a.width / 2;
+    int center_h = a.height / 2;
+
+    for (int y = 0; y < a.height; y++) {
+        png_bytep row_a = a.pixels[y];
+        png_bytep row_out = out.pixels[y];
+
+        for (int x = 0; x < a.width; x++) {
+            png_bytep px_a = &(row_a[x * 4]);
+            png_bytep px_out = &(row_out[x * 4]);
+
+            if ((x - center_w) * (x - center_w) + (y - center_h) * (y - center_h) <= radius * radius) {
+                for (int i = 0; i < 4; ++i)
+                    px_out[i] = px_a[i];
+            } else {
+                for (int i = 0; i < 4; ++i)
+                    px_out[i] = 0;
+            }
+        }
+    }
+
+    return OK;
+}
+
+int main(){
+    Image out;
+    Image a = read_png_file("file1.png");
+    Image b = read_png_file("file2.png");
+    Image mask = read_png_file("file3.png");
+
+    out.width = 1200;
+    out.height = 901;
+
+    blend(a, b, mask, out);
+    write_png_file(out, "out.png");
+
+    circle_image(a, out);
+    write_png_file(out, "out2.png");
 
     return 0;
 }
--- src/png_files.h
+++ src/png_files.h
@@ -3,76 +3,84 @@
 #include <stdio.h>
 #include <png.h>
 
-int width, height;
-png_byte color_type;
-png_byte bit_depth;
+using image_t = png_bytep*;
 
-png_bytep* read_png_file(char *filename) {
+struct Image{
+    int width, height;
+    png_byte color_type;
+    png_byte bit_depth;
+    image_t pixels = nullptr;
+
+    png_structp png;
+    png_infop info;
+};
+
+Image read_png_file(char *filename) {
+    Image out;
+
     FILE *fp = fopen(filename, "rb");
     
-    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if(!png) abort();
+    out.png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!out.png) abort();
     
-    png_infop info = png_create_info_struct(png);
-    if(!info) abort();
+    out.info = png_create_info_struct(out.png);
+    if(!out.info) abort();
     
-    if(setjmp(png_jmpbuf(png))) abort();
+    if(setjmp(png_jmpbuf(out.png))) abort();
     
-    png_init_io(png, fp);
+    png_init_io(out.png, fp);
     
-    png_read_info(png, info);
+    png_read_info(out.png, out.info);
     
-    width      = png_get_image_width(png, info);
-    height     = png_get_image_height(png, info);
-    color_type = png_get_color_type(png, info);
-    bit_depth  = png_get_bit_depth(png, info);
+    out.width      = png_get_image_width(out.png, out.info);
+    out.height     = png_get_image_height(out.png, out.info);
+    out.color_type = png_get_color_type(out.png, out.info);
+    out.bit_depth  = png_get_bit_depth(out.png, out.info);
 
-    printf("width: %d\nheight: %d\nbit_depth: %d\n", width, height, bit_depth);
+    printf("width: %d\nheight: %d\nbit_depth: %d\n", out.width, out.height, out.bit_depth);
     
     // Read any color_type into 8bit depth, RGBA format.
     // See http://www.libpng.org/pub/png/libpng-manual.txt
     
-    if(bit_depth == 16)
-      png_set_strip_16(png);
+    if(out.bit_depth == 16)
+      png_set_strip_16(out.png);
     
-    if(color_type == PNG_COLOR_TYPE_PALETTE)
-      png_set_palette_to_rgb(png);
+    if(out.color_type == PNG_COLOR_TYPE_PALETTE)
+      png_set_palette_to_rgb(out.png);
     
     // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
-    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-      png_set_expand_gray_1_2_4_to_8(png);
+    if(out.color_type == PNG_COLOR_TYPE_GRAY && out.bit_depth < 8)
+      png_set_expand_gray_1_2_4_to_8(out.png);
     
-    if(png_get_valid(png, info, PNG_INFO_tRNS))
-      png_set_tRNS_to_alpha(png);
+    if(png_get_valid(out.png, out.info, PNG_INFO_tRNS))
+      png_set_tRNS_to_alpha(out.png);
     
     // These color_type don't have an alpha channel then fill it with 0xff.
-    if(color_type == PNG_COLOR_TYPE_RGB ||
-       color_type == PNG_COLOR_TYPE_GRAY ||
-       color_type == PNG_COLOR_TYPE_PALETTE)
-      png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+    if(out.color_type == PNG_COLOR_TYPE_RGB ||
+       out.color_type == PNG_COLOR_TYPE_GRAY ||
+       out.color_type == PNG_COLOR_TYPE_PALETTE)
+      png_set_filler(out.png, 0xFF, PNG_FILLER_AFTER);
     
-    if(color_type == PNG_COLOR_TYPE_GRAY ||
-       color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-      png_set_gray_to_rgb(png);
+    if(out.color_type == PNG_COLOR_TYPE_GRAY ||
+       out.color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+      png_set_gray_to_rgb(out.png);
     
-    png_read_update_info(png, info);
+    png_read_update_info(out.png, out.info);
 
-    png_bytep* row_pointers = NULL;
+    out.pixels = (png_bytep*)malloc(sizeof(png_bytep) * out.height);
+    for(int y = 0; y < out.height; y++)
+        out.pixels[y] = (png_byte*)malloc(png_get_rowbytes(out.png,out.info));
+
+    printf("row: %d\n", png_get_rowbytes(out.png, out.info));
     
-    row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-    for(int y = 0; y < height; y++)
-        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
-    
-    png_read_image(png, row_pointers);
+    png_read_image(out.png, out.pixels);
     
     fclose(fp);
     
-    png_destroy_read_struct(&png, &info, NULL);
-
-    return row_pointers;
+    return out;
 }
 
-void write_png_file(png_bytep* row_pointers, char *filename) {
+void write_png_file(Image const& img, char *filename) {
     int y;
 
     FILE *fp = fopen(filename, "wb");
@@ -92,7 +100,7 @@ void write_png_file(png_bytep* row_pointers, char *filename) {
     png_set_IHDR(
       png,
       info,
-      width, height,
+      img.width, img.height,
       8,
       PNG_COLOR_TYPE_RGBA,
       PNG_INTERLACE_NONE,
@@ -105,19 +113,19 @@ void write_png_file(png_bytep* row_pointers, char *filename) {
     // Use png_set_filler().
     //png_set_filler(png, 0, PNG_FILLER_AFTER);
 
-    if (!row_pointers) abort();
+    if (!img.pixels) abort();
 
-    png_write_image(png, row_pointers);
+    png_write_image(png, img.pixels);
     png_write_end(png, NULL);
 
-    for(int y = 0; y < height; y++) {
-      free(row_pointers[y]);
+    for(int y = 0; y < img.height; y++) {
+      free(img.pixels[y]);
     }
-    free(row_pointers);
+    free(img.pixels);
 
     fclose(fp);
 
-    png_destroy_write_struct(&png, &info);
+    //png_destroy_write_struct(&png, &info);
 }
 
 /*
