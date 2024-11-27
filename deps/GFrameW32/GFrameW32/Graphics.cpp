cd5717d09b8ca23ab158d24ab0b8a4ac8179cb50
ka Kotey <newpontos@gmail.com>
2024-11-27 16:37:40 +0300

    lab 3

--- deps/GFrameW32/GFrameW32/Graphics.cpp
+++ deps/GFrameW32/GFrameW32/Graphics.cpp
@@ -7,12 +7,12 @@ const double M_PI = 3.1415926535897932384626433832795;
 #endif
 
 void DrawLine( int x0, int y0, int x1, int y1, RGBPIXEL color ) {
-    int x = x0, y = y0; //текущее значение точки, инициализируем координатами начальной точки
+    int x = x0, y = y0;
     int dx = x1 - x0, dy = y1 - y0;
-    int ix, iy; //величина приращений (-1, 0, 1) по координатам определяют направление движения
-    int e; //ошибка
-    int i; //счетчик цикла
-    //определение величины приращения по координатам, а также получение абсолютных значений dx, dy
+    int ix, iy;
+    int e;
+    int i;
+
     if (dx > 0) ix = 1;
     else if (dx < 0) { ix = -1; dx = -dx; }
     else ix = 0;
@@ -22,23 +22,22 @@ void DrawLine( int x0, int y0, int x1, int y1, RGBPIXEL color ) {
     else iy = 0;
 
     if (dx >= dy) {
-        e = 2 * dy - dx; //инициализация ошибки с поправкой на половину пиксела
-        if (iy >= 0) { //увеличиваем или не изменяем y
-            //основной цикл
-            for (i = 0; i <= dx; i++) {
-                gfSetPixel(x, y, color); // выводим точку
-                if (e >= 0) { //ошибка стала неотрицательной
-                    y += iy; //изменяем y
-                    e -= 2 * dx; //уменьшаем ошибку
-                }
-                x += ix; //всегда изменяем x
-                e += dy * 2; //и увеличиваем ошибку
-            }
-        }
-        else { //y уменьшается
+        e = 2 * dy - dx;
+        if (iy >= 0) {
             for (i = 0; i <= dx; i++) {
                 gfSetPixel(x, y, color);
-                if (e > 0) { // ошибка стала положительной. Условие изменилось с >= на >
+                if (e >= 0) {
+                    y += iy;
+                    e -= 2 * dx;
+                }
+                x += ix;
+                e += dy * 2;
+            }
+        }
+        else {
+            for (i = 0; i <= dx; i++) {
+                gfSetPixel(x, y, color);
+                if (e > 0) {
                     y += iy;
                     e -= 2 * dx;
                 }
@@ -48,23 +47,22 @@ void DrawLine( int x0, int y0, int x1, int y1, RGBPIXEL color ) {
         }
     }
     else {
-        e = 2 * dx - dy; //инициализация ошибки с поправкой на половину пиксела
-        if (ix >= 0) { //увеличиваем или не изменяем y
-            //основной цикл
-            for (i = 0; i <= dy; i++) {
-                gfSetPixel(x, y, color); // выводим точку
-                if (e >= 0) { //ошибка стала неотрицательной
-                    x += ix; //изменяем y
-                    e -= 2 * dy; //уменьшаем ошибку
-                }
-                y += iy; //всегда изменяем x
-                e += dx * 2; //и увеличиваем ошибку
-            }
-        }
-        else { //y уменьшается
+        e = 2 * dx - dy; 
+        if (ix >= 0) {
             for (i = 0; i <= dy; i++) {
                 gfSetPixel(x, y, color);
-                if (e > 0) { // ошибка стала положительной. Условие изменилось с >= на >
+                if (e >= 0) {
+                    x += ix; 
+                    e -= 2 * dy;
+                }
+                y += iy;
+                e += dx * 2; 
+            }
+        }
+        else { 
+            for (i = 0; i <= dy; i++) {
+                gfSetPixel(x, y, color);
+                if (e > 0) { 
                     x += ix;
                     e -= 2 * dy;
                 }
@@ -76,38 +74,38 @@ void DrawLine( int x0, int y0, int x1, int y1, RGBPIXEL color ) {
 }
 
 CLPointType Classify(double x1, double y1, double x2, double y2, double x, double y) {
-    double ax = x2 - x1; //a
+    double ax = x2 - x1;
     double ay = y2 - y1;
-    double bx = x - x1; //b
+    double bx = x - x1;
     double by = y - y1;
     double s = ax*by - bx*ay;
     if (s > 0) return LEFT;
     if (s < 0) return RIGHT;
-    if ((ax*bx < 0) || (ay*by < 0)) //противоположно направлению
-        return BEHIND; //позади
+    if ((ax*bx < 0) || (ay*by < 0))
+        return BEHIND;
     if ((ax*ax + ay*ay) < (bx*bx + by*by))
-        return BEHIND; //впереди
-    if (x1==x && y1==y) //совпадает с началом
+        return BEHIND;
+    if (x1==x && y1==y)
         return ORIGIN;
-    if (x2==x && y2==y) //совпадает с концом
+    if (x2==x && y2==y)
         return DESTINATION;
-    return BETWEEN; //между
+    return BETWEEN;
 }
 
 EType EdgeType(double ox, double oy, double dx, double dy, double ax, double ay) {
     switch(Classify(ox, oy, dx, dy, ax, ay)) {
         case LEFT:
-            if(ay > oy && ay <= dy) return CROSS_LEFT; // пересекающая, A слева
-            else return INESSENTIAL; // безразличная
+            if(ay > oy && ay <= dy) return CROSS_LEFT;
+            else return INESSENTIAL;
         case RIGHT:
-            if(ay > dy && ay <= oy) return CROSS_RIGHT; // пересекающая, A справа
-            else return INESSENTIAL; // безразличная
+            if(ay > dy && ay <= oy) return CROSS_RIGHT;
+            else return INESSENTIAL;
         case BETWEEN:
         case ORIGIN:
         case DESTINATION:
-            return TOUCHING; // касающаяся
+            return TOUCHING;
         default:
-            return INESSENTIAL; // безразличная
+            return INESSENTIAL;
     }
 }
 
@@ -115,14 +113,14 @@ PType PInPolygonEOMode(double x, double y, const point *p, int n) {
     int param = 0;
     for(int i = 0; i < n; i++) {
         switch(EdgeType(p[i].x, p[i].y, p[(i+1)%n].x, p[(i+1)%n].y, x, y)) {
-            case TOUCHING: //если лежит на полигоне, то заведомо принадлежит
+            case TOUCHING:
                 return INSIDE;
             case CROSS_LEFT:
             case CROSS_RIGHT:
-                param = 1 - param; //изменяем значение четности
+                param = 1 - param;
         }
     }
-    if (param == 1) return INSIDE; //нечетное
+    if (param == 1) return INSIDE;
     else return OUTSIDE;
 }
 
@@ -130,12 +128,16 @@ PType PInPolygonNZWMode(double x, double y, const point* p, int n) {
     int param = 0;
     for (int i = 0; i < n; i++) {
         switch (EdgeType(p[i].x, p[i].y, p[(i + 1) % n].x, p[(i + 1) % n].y, x, y)) {
-            case TOUCHING: //если лежит на полигоне, то заведомо принадлежит
+            case TOUCHING:
                 return INSIDE;
             case CROSS_LEFT:
                 ++param;
+                break;
             case CROSS_RIGHT:
                 --param;
+                break;
+            default:
+                break;
         }
     }
     if (param != 0) return INSIDE;
@@ -169,9 +171,10 @@ void gfDrawPolygon(std::vector<point> const& points, PType(*inFucn)(double, doub
 bool gfInitScene(){
     gfSetWindowSize( 640, 480 );
     
+    std::vector<point> star{ {100, 400}, {250, 100}, {400, 400}, {80, 150}, {420, 150} };
+
     DrawLine(300, 300, 500, 0, RGBPIXEL::Green());
-    gfDrawPolygon({ {100, 400}, {250, 100}, {400, 400}   }, PInPolygonNZWMode);
-    //gfDrawPolygon({ {100, 400}, {250, 100}}, PInPolygonNZWMode);
+    gfDrawPolygon(star, PInPolygonEOMode);
 
     return true;
 }
